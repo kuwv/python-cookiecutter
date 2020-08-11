8f31569405f84f8b3289e8aad2bc96d03ed90f1c
kuwv <kuwv@localhost.localdomain>
2020-08-10 20:47:05 -0400

    Update variables

---
+++ hooks/post_gen_project.py
@@ -0,0 +1,16 @@
+'''Run cookiecutter post script.'''
+import os
+import shutil
+
+def remove(filepath):
+    '''Remove selected directory / files.'''
+    if os.path.isdir(filepath):
+        shutil.rmtree(filepath)
+    if os.path.isfile(filepath):
+        os.remove(filepath)
+
+if "{{ cookiecutter.package_manager }}" == 'poetry':
+    remove('Pipfile')
+elif "{{ cookiecutter.package_manager }}" == 'pipenv':
+    remove('pyproject.toml')
+
--- {{ cookiecutter.project_slug }}/README.md
+++ {{ cookiecutter.project_slug }}/README.md
@@ -1,4 +1,5 @@
 # {{ cookiecutter.project_name }}
+{{ cookiecutter.django_template }}
 
 [![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
 [![Build Status](https://travis-ci.org/{{ cookiecutter.username }}/{{ cookiecutter.project_slug }}.svg?branch=master)](https://travis-ci.org/{{ cookiecutter.username}}/{{ cookiecutter.project_slug }})
