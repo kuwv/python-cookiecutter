133c91733ba8a281d92d19a5d85f09adf0372c86
Jesse P. Johnson <jpj6652@gmail.com>
2021-08-01 10:43:45 -0400

    refactor: license

--- {{ cookiecutter.project_slug }}/{{ cookiecutter.module_name }}/__init__.py
+++ {{ cookiecutter.project_slug }}/{{ cookiecutter.module_name }}/__init__.py
@@ -1,13 +1,14 @@
 # -*- coding: utf-8 -*-
-'''{{ cookiecutter.project_description }}'''
+# copyright: Copyright (C) [2021] [{{ cookiecutter.author }}]
+# license: {{ cookiecutter.license }}, see LICENSE for more details.
 
 import logging
-
-logging.getLogger(__name__).addHandler(logging.NullHandler())
 
 __author__ = '{{ cookiecutter.author }}'
 __title__ = '{{ cookiecutter.module_name }}'
 __version__ = '0.1.0'
-__license__ = 'Apache-2.0'
+__license__ = '{{ cookiecutter.license }}'
+
+logging.getLogger(__name__).addHandler(logging.NullHandler())
 
 __all__ = []
