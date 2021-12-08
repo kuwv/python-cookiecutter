5f4d7b50305cd83fb56892e88c9aefed4a008aaf
Jesse P. Johnson <jpj6652@gmail.com>
2021-12-08 00:11:07 -0500

    refactor: cleanup cruft

--- {{ cookiecutter.project_slug }}/src/{{ cookiecutter.module_name }}/__init__.py
+++ {{ cookiecutter.project_slug }}/src/{{ cookiecutter.module_name }}/__init__.py
@@ -3,11 +3,12 @@
 # license: {{ cookiecutter.license }}, see LICENSE for more details.
 
 import logging
+from typing import Tuple
 
 __author__ = '{{ cookiecutter.author }}'
 __title__ = '{{ cookiecutter.module_name }}'
 __version__ = '0.1.0'
 __license__ = '{{ cookiecutter.license }}'
-__all__ = []
+__all__: Tuple[str, ...] = ()
 
 logging.getLogger(__name__).addHandler(logging.NullHandler())
