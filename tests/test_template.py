32424340e3befee1c7bb1df4b550db381de77b6e
Jesse P. Johnson <jpj6652@gmail.com>
2022-04-10 11:34:48 -0400

    style: update strings

--- tests/test_template.py
+++ tests/test_template.py
@@ -1,28 +1,28 @@
-'''Test template deployment.'''
+"""Test template deployment."""
 import pytest
 
 
 @pytest.fixture
 def custom_template(tmpdir):
-    '''Configure template fixture.'''
+    """Configure template fixture."""
     template = tmpdir.ensure('cookiecutter-template', dir=True)
-    template.join('cookiecutter.json').write('''\
+    template.join('cookiecutter.json').write("""\
         {
-            "author": "test",
-            "email": "test@localhost",
-            "account": "test",
-            "project_name": "example-project"
+            'author': 'test',
+            'email': 'test@localhost',
+            'account': 'test',
+            'project_name': 'example-project'
         }
-    ''')
+    """)
 
-    repo_dir = template.ensure("{{ cookiecutter.project_name }}", dir=True)
-    repo_dir.join('README.md').write("{{ cookiecutter.project_name }}")
+    repo_dir = template.ensure('{{ cookiecutter.project_name }}', dir=True)
+    repo_dir.join('README.md').write('{{ cookiecutter.project_name }}')
 
     return template
 
 
 def test_bake_custom_project(cookies, custom_template):
-    '''Test for cookiecutter-template.'''
+    """Test for cookiecutter-template."""
     result = cookies.bake(template=str(custom_template))
 
     assert result.exit_code == 0
