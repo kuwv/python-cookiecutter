f40006a1c0874c060174c138ecda5bec02810206
Jesse P. Johnson <jpj6652@gmail.com>
2022-09-14 20:11:26 -0400

    feat: add pep621 support

--- {{ cookiecutter.project_slug }}/src/{{ cookiecutter.module_name }}/__init__.py
+++ {{ cookiecutter.project_slug }}/src/{{ cookiecutter.module_name }}/__init__.py
@@ -7,7 +7,7 @@ from typing import List
 __author__ = '{{ cookiecutter.author }}'
 __title__ = '{{ cookiecutter.module_name }}'
 __version__ = '0.1.0a0'
-{%- if cookiecutter.license == 'MPL-2.0' %}
+{%- if cookiecutter.license != 'Unlicensed' %}
 __license__ = '{{ cookiecutter.license }}'
 {%- endif %}
 __all__: List[str] = []
